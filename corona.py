8e8824bcf4d6a79e9e8f257d5c1a67f3d9c7c8b9
ShepelIlya <chepsiilya@gmail.com>
2020-03-31 18:14:14 +0300

    .

--- corona.py
+++ corona.py
@@ -1,25 +1,29 @@
 import numpy as np
 from scipy import interpolate
+import matplotlib.pyplot as plt
 
-x = np.arange(0,24)
+
+x = np.arange(0,30)
 x_last = x[:-1]
 y = np.array([
     1, 1, 1, 1, 7, 11, 14,
     17, 20, 28, 34, 45, 59, 63,
     93, 114, 147, 199, 253, 306, 367,
-    438, 495, 658
+    438, 495, 658, 840, 1036, 1264, 1534,
+    1836, 2337
 ])
-f = interpolate.interp1d(x, y, fill_value='extrapolate')
-
-y_diff = y[1:] - y[:-1]
-f_diff = interpolate.interp1d(x[:-1], y_diff, fill_value='extrapolate')
 
 if __name__ == '__main__':
-    # print(y_diff)
-    f_23 = interpolate.interp1d(x[:-1], y[:-1], fill_value='extrapolate')
-    # print(f_23(24))
+    f = interpolate.interp1d(x, y, fill_value='extrapolate', kind='quadratic')
 
-    f_23_diff = interpolate.interp1d(x_last[:-1], y_diff[:-1], fill_value='extrapolate')
+    y_diff = y[1:] - y[:-1]
+    f_diff = interpolate.interp1d(x[:-1], y_diff, fill_value='extrapolate', kind='quadratic')
 
-    # print(f_23_diff(23))
-    # print("f_diff: " , f_diff(24))
+    x_gr = np.arange(0, len(x) + 28)
+    y_gr = [f(x) for x in x_gr]
+
+    fig = plt.figure()
+    plt.plot(x_gr, y_gr)
+    plt.show()
+
+    
